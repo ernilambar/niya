bc63f9c24113780c7edb561f074cf3e0f2682135
Nilambar Sharma <nilambar@outlook.com>
2023-09-12 10:33:53 +0545

    Setup project

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @package Niya
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+
+define( 'NIYA_VERSION', '1.0.0' );
+define( 'NIYA_DIR', rtrim( get_template_directory(), '/' ) );
+define( 'NIYA_URL', rtrim( get_template_directory_uri(), '/' ) );
