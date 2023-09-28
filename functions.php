183735543f4d541ba225d75ed68e3b263d7bcf4a
Nilambar Sharma <nilambar@outlook.com>
2023-09-28 11:37:48 +0545

    Fix URL

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit( 'Direct script access denied.' );
 }
 
-define( 'NIYA_VERSION', '1.0.0' );
+define( 'NIYA_VERSION', '1.0.1' );
 define( 'NIYA_DIR', rtrim( get_template_directory(), '/' ) );
 define( 'NIYA_URL', rtrim( get_template_directory_uri(), '/' ) );
 
